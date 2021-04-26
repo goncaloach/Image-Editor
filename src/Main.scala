9d8a5ddc92791fd003449f9f53e96573f2e669cd
goncaloach <goncaloach@gmail.com>
2021-04-26 17:49:05 +0100

    n impar pix fixed 2.0

--- src/Main.scala
+++ src/Main.scala
@@ -4,6 +4,7 @@ object Main extends QuadTree {
 
   def main(args: Array[String]): Unit = {
 
+
     val qt = makeQTree(ImageUtil.readColorImage("objc2_2.png").toList map (x=> x.toList))
     println(qt)
 
@@ -16,13 +17,13 @@ object Main extends QuadTree {
     val guardarImagem = ImageUtil.writeImage(bm.toArray map (x=> x.toArray),"output.png","png")
     */
 
-    /*val l1: QLeaf[Coords, Section] = QLeaf((((0,0):Point,(0,0):Point):Coords, Color.red):Section)
+    val l1: QLeaf[Coords, Section] = QLeaf((((0,0):Point,(0,0):Point):Coords, Color.red):Section)
     val l2: QLeaf[Coords, Section] = QLeaf((((1,0):Point,(1,0):Point):Coords, Color.blue):Section)
     val l3: QLeaf[Coords, Section] = QLeaf((((0,1):Point,(0,1):Point):Coords, Color.yellow):Section)
     val l4: QLeaf[Coords, Section] = QLeaf((((1,1):Point,(1,1):Point):Coords, Color.green):Section)
 
     val qt2: QTree[Coords] = QNode(((0,0),(1,1)), l1, l2, l3, l4)
-    println(qt2)*/
+    println(qt2)
   }
 
 }
