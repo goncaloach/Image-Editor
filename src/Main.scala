3d83576c77047cb2df40be6430bf8b421b36a632
goncaloach <goncaloach@gmail.com>
2021-05-01 01:17:18 +0100

    pure? NOIDONTTHINKSO

--- src/Main.scala
+++ src/Main.scala
@@ -1,26 +1,29 @@
+import java.awt.Color
+
 object Main {
 
   def main(args: Array[String]): Unit = {
     //val nomeFicheiro = "3x1"
     //val quad = new QuadTree(ImageUtil.readColorImage(nomeFicheiro+".png").toList map (x=> x.toList))
 
-
-    val quad = new QuadTree(ImageUtil.readColorImage("3x2g.png").toList map (x=> x.toList))
-    //println(quad)
+    val quad = new QuadTree(ImageUtil.readColorImage("lul.png").toList map (x=> x.toList))
     val qt = quad.makeQTree()
-    //println(qt)
+
 
     //val scaled = quad.scale(2,qt)
     //println(scaled)
 
-    val eff = quad.mapColourEffect(c => c,qt)
+    //val sepia = quad.applySepia(qt)
+    //val contrast = quad.applyContrast(qt)
+    //val noise = quad.applyNoise(qt)
+    val noisePure = quad.applyNoisePure(qt)
 
 
-    val bm = quad.QTreeToBitMap(eff)
-    val guardarImagem = ImageUtil.writeImage(bm.toArray map (x=> x.toArray),"output.png","png")
+    val bm = quad.QTreeToBitMap(noisePure)
+    ImageUtil.writeImage(bm.toArray map (x=> x.toArray),"output.png","png")
 
 
-    //val guardarImagem = ImageUtil.writeImage(bm.toArray map (x=> x.toArray),"new"+nomeFicheiro+".png","png")
+    //ImageUtil.writeImage(bm.toArray map (x=> x.toArray),"new"+nomeFicheiro+".png","png")
   }
 
 }
--- src/QuadTree.scala
+++ src/QuadTree.scala
@@ -11,11 +11,18 @@ case class QuadTree(bitMap: BitMap) {
 
   def scale[A](value:Double,qt:QTree[A]):QTree[Coords]=QuadTree.scale(value,qt)
 
-  def rotateL[A](qt:QTree[A]):QTree[Any]=QuadTree.rotateL(qt)
+  def rotateL[A](qt:QTree[A]):QTree[Coords]=QuadTree.rotateL(qt)
 
-  def rotateR[A](qt:QTree[A]):QTree[Any]=QuadTree.rotateR(qt)
+  def rotateR[A](qt:QTree[A]):QTree[Coords]=QuadTree.rotateR(qt)
 
-  def mapColourEffect[A](f:Color => Color, qt:QTree[A]):QTree[Coords]=QuadTree.mapColourEffect(f,qt)
+  def applySepia[A](qt: QTree[A]):QTree[Coords]=QuadTree.applySepia(qt)
+
+  def applyContrast[A](qt:QTree[A]):QTree[Coords]=QuadTree.applyContrast(qt)
+
+  def applyNoise[A](qt:QTree[A]):QTree[Coords]=QuadTree.applyNoise(qt)
+
+  def applyNoisePure[A](qt:QTree[A]):QTree[Coords]=QuadTree.applyNoisePure(qt)
+
 }
 
 object QuadTree{
@@ -35,15 +42,12 @@ object QuadTree{
         case h :: t => h == t.head && isSameColor(t)
       }
     }
-
     lst match {
       case List() => true
       case List(h) => isSameColor(h)
       case h::t => isSameColor(h) == isSameColor(t.head) && h.head==t.head.head && isSameColor2D(t)
     }
   }
-
-  ///////
 
   def makeQTree(b: BitMap): QTree[Coords] = {
 
@@ -57,12 +61,10 @@ object QuadTree{
           } else{
             val altura = lst.length
             val comprimento = lst.head.length
-
             val topLeft = lst.slice(0, altura/2)          map (x => x.slice(0, comprimento/2) )
             val topRight = lst.slice(0, altura/2)         map (x => x.slice(comprimento/2, comprimento))
             val botLeft = lst.slice(altura/2,altura)      map (x => x.slice(0, comprimento/2))
             val botRight = lst.slice(altura/2,altura)     map (x => x.slice(comprimento/2, comprimento))
-
             QNode(((x1, y1), (x2, y2)),
               divide(topLeft, x1, y1, x2-math.ceil(comprimento/2f).toInt, y2-math.ceil(altura/2f).toInt),
               divide(topRight, x1+math.floor(comprimento/2f).toInt, y1, x2, y2-math.ceil(altura/2f).toInt),
@@ -128,32 +130,118 @@ object QuadTree{
 
   //////////////////////////////////////////////////////////////////////////////////
 
-  def rotateR[A](qt: QTree[A]): QTree[A] = qt match {
-    case QEmpty => QEmpty
-    case QLeaf(value) => QLeaf(value)
-    case QNode(root, one, two, three, four) => QNode(root, rotateR(three), rotateR(one), rotateR(four), rotateR(two))
+  def rotateR[A](qt: QTree[A]): QTree[Coords] = {
+    qt match {
+      case QEmpty => QEmpty
+      case QLeaf(value) => QLeaf(value)
+      case QNode(root:Coords, one, two, three, four) =>
+        QNode(root:Coords, rotateR(three), rotateR(one), rotateR(four), rotateR(two))
+    }
   }
 
-  def rotateL[A](qt: QTree[A]): QTree[A] = qt match {
-    case QEmpty => QEmpty
-    case QLeaf(value) => QLeaf(value)
-    case QNode(root, one, two, three, four) => QNode(root, rotateL(two), rotateL(four), rotateL(one), rotateL(three))
+  def rotateL[A](qt: QTree[A]): QTree[Coords] = {
+    qt match {
+      case QEmpty => QEmpty
+      case QLeaf(value) => QLeaf(value)
+      case QNode(root:Coords, one, two, three, four) =>
+        QNode(root, rotateL(two), rotateL(four), rotateL(one), rotateL(three))
+    }
   }
+
+  ////////////////////////////////////////////////////////////////////////////////////
 
   def mapColourEffect[A](f:Color => Color, qt:QTree[A]):QTree[Coords]={
     qt match {
       case QEmpty => QEmpty
-      case QLeaf((cords : Coords,cor:Int)) => QLeaf(cords,f)
-      case QNode(_,one,two,three,four) =>
-        mapColourEffect(f,one)
-        mapColourEffect(f,two)
-        mapColourEffect(f,three)
-        mapColourEffect(f,four)
+      case QLeaf((cords : Coords,cor:Int)) => QLeaf(cords,f(HCtoColor(cor)).hashCode())
+      case QNode(root:Coords,one,two,three,four) => QNode(root, mapColourEffect(f,one), mapColourEffect(f,two),
+                                                                mapColourEffect(f,three), mapColourEffect(f,four))
     }
   }
 
+  def mapColourEffect_1[A](rndm: RandomWithState, f:(Color,RandomWithState) => (Color,RandomWithState), qt:QTree[A]):(QTree[Coords],RandomWithState)={
+    val next = rndm.nextInt(2)
+    val next1 = next._2.nextInt(234)
+    val next2 = next1._2.nextInt(5685678)
+    val next3 = next2._2.nextInt(234764)
+    qt match {
+      case QEmpty => (QEmpty,rndm)
+      case QLeaf((cords : Coords,cor:Int)) =>
+        val res:(Color,RandomWithState) = f(HCtoColor(cor),next._2)
+        (QLeaf(cords,res._1.hashCode()),res._2)
+      case QNode(root:Coords,one,two,three,four) => (QNode(root,mapColourEffect_1(next._2,f,one)._1, mapColourEffect_1(next1._2,f,two)._1,
+        mapColourEffect_1(next2._2,f,three)._1, mapColourEffect_1(next3._2,f,four)._1),rndm)
+    }
+  }
 
+  private def HCtoColor(hash : Int):Color={  //hashcode to color
+    val lst = ImageUtil.decodeRgb(hash).toList
+    new Color(lst.head,lst.tail.head,lst.last)
+  }
 
+  private def truncate(valor : Int):Int={
+    Math.max(Math.min(255,valor),0)
+  }
+
+  private def sepiaEffect(clr : Color):Color={
+    val red = clr.getRed
+    val green = clr.getGreen
+    val blue = clr.getBlue
+    val newRed = truncate(((red * 0.393) + (green *0.769) + (blue * 0.189)).toInt)
+    val newGreen = truncate(((red * 0.349) + (green *0.686) + (blue * 0.168)).toInt)
+    val newBlue = truncate(((red * 0.272) + (green *0.534) + (blue * 0.131)).toInt)
+    new Color(newRed,newGreen,newBlue)
+  }
+
+  private def contrastEffect(clr : Color):Color={ //falta mudar o 128 para parametro de entrada
+    def factor(contrast : Int):Int={
+      (259 * (contrast + 255)) / (255 * (259 - contrast))
+    }
+    val newRed   = truncate(factor(128) * (clr.getRed   - 128) + 128)
+    val newGreen = truncate(factor(128) * (clr.getGreen - 128) + 128)
+    val newBlue  = truncate(factor(128) * (clr.getBlue  - 128) + 128)
+    new Color(newRed,newGreen,newBlue)
+  }
+
+  private def noiseEffect(clr:Color):Color={
+    val rndm = new scala.util.Random
+    rndm.nextInt(2) match{
+      case 0 => clr
+      case 1 =>  Color.gray        //noise with gray
+      /*val r = rndm.nextInt(256)  //noise with random color
+      val g = rndm.nextInt(256)
+      val b = rndm.nextInt(256)
+      new Color(r,g,b)*/
+    }
+  }
+
+  private def noiseEffectPure(clr:Color, random:RandomWithState):(Color,RandomWithState)={
+    val bool = random.nextInt(2)
+    bool._1 match{
+      case 0 => (clr,bool._2)
+      case 1 => //(Color.gray,bool._2)  //noise with gray
+        val r = bool._2.nextInt(256)     //noise with random color
+        val g = r._2.nextInt(256)
+        val b = g._2.nextInt(256)
+        (new Color(r._1,g._1,b._1),b._2)
+    }
+  }
+
+  def applySepia[A](qt: QTree[A]):QTree[Coords]={
+    mapColourEffect(sepiaEffect,qt)
+  }
+
+  def applyContrast[A](qt:QTree[A]):QTree[Coords]={
+    mapColourEffect(contrastEffect,qt)
+  }
+
+  def applyNoise[A](qt:QTree[A]):QTree[Coords]={
+    mapColourEffect(noiseEffect,qt)
+  }
+
+  def applyNoisePure[A](qt:QTree[A]):QTree[Coords]={
+    mapColourEffect_1(MyRandom(12234),noiseEffectPure,qt)._1
+  }
 
 }
 
