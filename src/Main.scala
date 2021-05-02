d4b093bb28580b0a1005fbe0aa60916d03f11d50
goncaloach <goncaloach@gmail.com>
2021-05-03 00:31:26 +0100

    bug no makeQTree 1x3

--- src/Main.scala
+++ src/Main.scala
@@ -1,35 +1,38 @@
 import QuadTree.Coords
 
 import java.awt.Color
+import scala.annotation.tailrec
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    //val nomeFicheiro = "3x1"
+    //val nomeFicheiro = "lul"
     //val quad = new QuadTree(ImageUtil.readColorImage(nomeFicheiro+".png").toList map (x=> x.toList))
 
-    val quad = new QuadTree(ImageUtil.readColorImage("lul.png").toList map (x=> x.toList))
+    val quad = new QuadTree(ImageUtil.readColorImage("1x3.png").toList map (x=> x.toList))
+    println(quad)
     val qt = quad.makeQTree()
-
+    println(qt)
 
     //val scaled = quad.scale(2,qt)
     //println(scaled)
 
+    //val mirrorH = quad.mirrorH(qt)
+    //val mirrorV = quad.mirrorV(qt)
+
     val rotateR = quad.rotateR(qt)
+    println(rotateR)
+    //val rotateL = quad.rotateL(qt)
 
     //val sepia = quad.applySepia(qt)
-    //val contrast = quad.applyContrast(qt)
+    //val contrast = quad.applyContrast(qt,256)
     //val noise = quad.applyNoise(qt)
     //val noisePure = quad.applyNoisePure(qt)
 
 
-
-
-
-
-
-    val bm = quad.QTreeToBitMap(rotateR)
+    val bm = quad.QTreeToBitMap(qt)
     ImageUtil.writeImage(bm.toArray map (x=> x.toArray),"output.png","png")
+
 
 
     //ImageUtil.writeImage(bm.toArray map (x=> x.toArray),"new"+nomeFicheiro+".png","png")
--- src/QuadTree.scala
+++ src/QuadTree.scala
@@ -11,13 +11,17 @@ case class QuadTree(bitMap: BitMap) {
 
   def scale[A](value:Double,qt:QTree[A]):QTree[Coords]=QuadTree.scale(value,qt)
 
+  def mirrorH[A](qt: QTree[Coords]): QTree[Coords] =QuadTree.mirrorH(qt)
+
+  def mirrorV[A](qt:QTree[A]):QTree[Coords]=QuadTree.mirrorV(qt)
+
   def rotateL[A](qt:QTree[A]):QTree[Coords]=QuadTree.rotateL(qt)
 
   def rotateR[A](qt:QTree[A]):QTree[Coords]=QuadTree.rotateR(qt)
 
   def applySepia[A](qt: QTree[A]):QTree[Coords]=QuadTree.applySepia(qt)
 
-  def applyContrast[A](qt:QTree[A]):QTree[Coords]=QuadTree.applyContrast(qt)
+  def applyContrast[A](qt:QTree[A],value:Int):QTree[Coords]=QuadTree.applyContrast(qt,value)
 
   def applyNoise[A](qt:QTree[A]):QTree[Coords]=QuadTree.applyNoise(qt)
 
@@ -31,6 +35,8 @@ object QuadTree{
   type Coords = (Point, Point)
   type Section = (Coords, Color)
   type BitMap = List[List[Int]]
+
+  //////////////////////////////////////// T1 (1) ///////////////////////////////////////////
 
   @tailrec
   private def isSameColor2D(lst:List[List[Int]]):Boolean={
@@ -51,13 +57,13 @@ object QuadTree{
 
   def makeQTree(b: BitMap): QTree[Coords] = {
     def divide(lst: List[List[Int]],x1: Int,y1: Int,x2: Int,y2: Int): QTree[Coords] = {
+      println(lst)
       lst match {
         case List(List())  => QEmpty
+        case List(List(), List())  => QEmpty
         case List()  => QEmpty
-        case h::t =>
-          if (isSameColor2D(lst)) {
-            QLeaf(((x1, y1),(x2, y2)), lst.head.head)
-          } else{
+        case h::t => if (isSameColor2D(lst))  QLeaf(((x1, y1),(x2, y2)), lst.head.head)
+          else{
             val altura = lst.length
             val comprimento = lst.head.length
             val topLeft = lst.slice(0, altura/2)          map (x => x.slice(0, comprimento/2) )
@@ -66,16 +72,16 @@ object QuadTree{
             val botRight = lst.slice(altura/2,altura)     map (x => x.slice(comprimento/2, comprimento))
             QNode(((x1, y1), (x2, y2)),
               divide(topLeft, x1, y1, x2-math.ceil(comprimento/2f).toInt, y2-math.ceil(altura/2f).toInt),
-              divide(topRight, x1+math.floor(comprimento/2f).toInt, y1, x2, y2-math.ceil(altura/2f).toInt),
-              divide(botLeft, x1, y1+math.floor(altura/2f).toInt, x2-math.ceil(comprimento/2f).toInt, y2),
-              divide(botRight, x1+math.floor(comprimento/2f).toInt, y1+math.floor(altura/2f).toInt, x2, y2))
+              divide(topRight, x1+comprimento/2, y1, x2, y2-math.ceil(altura/2f).toInt),
+              divide(botLeft, x1, y1+altura/2, x2-math.ceil(comprimento/2f).toInt, y2),
+              divide(botRight, x1+comprimento/2, y1+altura/2, x2, y2))
           }
       }
     }
     divide(b,0,0,b.head.length-1,b.length-1)
   }
 
-  //////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////// T1 (2) //////////////////////////////////////////
 
   def QTreeToBitMap[A](qt: QTree[A]):BitMap = {
     qt match{
@@ -90,6 +96,8 @@ object QuadTree{
   }
 
   private def concatListH(lst1 : List[List[Int]],lst2:List[List[Int]]):List[List[Int]]= {
+    println("list 1"+lst1)
+    println("list2 " +lst2)
     (lst1, lst2) match {
       case (List(), List()) => List()
       case (Nil, List(h2)) => List(h2)
@@ -102,7 +110,7 @@ object QuadTree{
     list1 ::: list2
   }
 
-  //////////////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////// T2 //////////////////////////////////////////////
 
   def scale[A](valor: Double, qt: QTree[A]): QTree[Coords]={
 
@@ -125,56 +133,105 @@ object QuadTree{
     }
   }
 
-  //////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////// T3,T4 ///////////////////////////////////////
 
-  private def changeCoordsQT(cords:Coords,alt:Int,larg:Int):Coords = {
-    val x1 = cords._1._1
-    val y1 = cords._1._2
-    val x2 = cords._2._1
-    val y2 = cords._2._2
-    ((y1, x1 - larg), (y2, x2 - larg))
+  private def getAltQT[A](qt:QTree[A]):Int={
+    qt match {
+      case QEmpty=>0
+      case QLeaf((cords:Coords,_)) => cords._2._2
+      case QNode(cords:Coords,_,_,_,_) => cords._2._2
+    }
   }
 
   private def getCompQT[A](qt:QTree[A]):Int={
     qt match {
-      case QEmpty=> 0
-      case QLeaf(cords: Coords) => cords._2._1+1
-      case QNode(cords:Coords,_,_,_,_)=> cords._2._1+1
+      case QEmpty=>0
+      case QLeaf((cords:Coords,_)) => cords._2._1
+      case QNode(cords:Coords,_,_,_,_) => cords._2._1
     }
   }
 
-  private def getAltQT[A](qt:QTree[A]):Int={
-    qt match {
-      case QEmpty=> 0
-      case QLeaf(cords: Coords) => cords._2._2+1
-      case QNode(cords:Coords,_,_,_,_)=> cords._2._2+1
-    }
-  }
-
-  def rotateR[A](qt: QTree[A]): QTree[Coords] = {
-    val larg = getCompQT(qt)
-    val alt = getAltQT(qt)
+  private def modifyImage[A](qt: QTree[A],f1:Coords=>Coords,f2:QNode[Coords]=>QTree[Coords]):QTree[Coords] = {
     def aux[A](q: QTree[A]): QTree[Coords] = {
       q match {
         case QEmpty => QEmpty
-        case QLeaf((cords: Coords, color: Int)) => QLeaf((changeCoordsQT(cords,larg,alt), color: Int))
-        case QNode(root: Coords, one, two, three, four) =>
-          QNode(changeCoordsQT(root,larg,alt), aux(three), aux(one), aux(four), aux(two))
+        case QLeaf((cords: Coords, color: Int)) => QLeaf((f1(cords), color))
+        case QNode(root: Coords, one:QTree[Coords], two:QTree[Coords], three:QTree[Coords], four:QTree[Coords]) =>
+          f2(QNode(root: Coords, aux(one), aux(two), aux(three), aux(four)))
       }
     }
     aux(qt)
   }
 
-  def rotateL[A](qt: QTree[A]): QTree[Coords] = {  //not done
-    qt match {
-      case QEmpty => QEmpty
-      case QLeaf(value) => QLeaf(value)
-      case QNode(root:Coords, one, two, three, four) =>
-        QNode(root, rotateL(two), rotateL(four), rotateL(one), rotateL(three))
-    }
+  private def mirrorHCords(comp:Int)(cords:Coords):Coords = {
+    val x1 = cords._1._1
+    val y1 = cords._1._2
+    val x2 = cords._2._1
+    val y2 = cords._2._2
+    ((comp-x2,y1), (comp-x1,y2))
   }
 
-  ////////////////////////////////////////////////////////////////////////////////////
+  private def mirrorHQNode(comp:Int)(qn:QNode[Coords]):QTree[Coords]={
+    QNode(mirrorHCords(comp)(qn.value), qn.two, qn.one, qn.four, qn.three)
+  }
+
+  private def mirrorVCords(alt:Int)(cords:Coords):Coords = {
+    val x1 = cords._1._1
+    val y1 = cords._1._2
+    val x2 = cords._2._1
+    val y2 = cords._2._2
+    ((x1,alt-y2), (x2,alt-y1))
+  }
+
+  private def mirrorVQNode(alt:Int)(qn:QNode[Coords]):QTree[Coords]={
+    QNode(mirrorHCords(alt)(qn.value), qn.three, qn.four, qn.one, qn.two)
+  }
+
+  private def rotateLCords(comp:Int)(cords:Coords):Coords = {
+    val x1 = cords._1._1
+    val y1 = cords._1._2
+    val x2 = cords._2._1
+    val y2 = cords._2._2
+    ((y1,comp-x2), (y2,comp-x1))
+  }
+
+  private def rotateLQNode(comp:Int)(qn:QNode[Coords]):QTree[Coords]={
+    QNode(rotateLCords(comp)(qn.value), qn.two, qn.four, qn.one, qn.three)
+  }
+
+  private def rotateRCords(alt:Int)(cords:Coords):Coords = {
+    val x1 = cords._1._1
+    val y1 = cords._1._2
+    val x2 = cords._2._1
+    val y2 = cords._2._2
+    ((alt-y2,x1), (alt-y1,x2))
+  }
+
+  private def rotateRQNode(alt:Int)(qn:QNode[Coords]):QTree[Coords]={
+    QNode(rotateRCords(alt)(qn.value), qn.three, qn.one, qn.four, qn.two)
+  }
+
+  def mirrorH[A](qt:QTree[A]):QTree[Coords]={
+    val comp = getCompQT(qt)
+    modifyImage(qt,mirrorHCords(comp),mirrorHQNode(comp))
+  }
+
+  def mirrorV[A](qt:QTree[A]):QTree[Coords]={
+    val alt = getAltQT(qt)
+    modifyImage(qt,mirrorVCords(alt),mirrorVQNode(alt))
+  }
+
+  def rotateL[A](qt:QTree[A]):QTree[Coords]={
+    val comp = getCompQT(qt)
+    modifyImage(qt,rotateLCords(comp),rotateLQNode(comp))
+  }
+
+  def rotateR[A](qt:QTree[A]):QTree[Coords]={
+    val alt = getAltQT(qt)
+    modifyImage(qt,rotateRCords(alt),rotateRQNode(alt))
+  }
+
+  ///////////////////////////////////////// T5 ///////////////////////////////////////////
 
   private def mapColourEffect[A](f:Color => Color, qt:QTree[A]):QTree[Coords]={
     qt match {
@@ -195,8 +252,10 @@ object QuadTree{
       case QLeaf((cords : Coords,cor:Int)) =>
         val res:(Color,RandomWithState) = f(HCtoColor(cor),next.nextInt._2)
         (QLeaf(cords,res._1.hashCode()),res._2)
-      case QNode(root:Coords,one,two,three,four) => (QNode(root,mapColourEffect_1(next.nextInt._2,f,one)._1, mapColourEffect_1(next1.nextInt._2,f,two)._1,
-        mapColourEffect_1(next2.nextInt._2,f,three)._1, mapColourEffect_1(next3.nextInt._2,f,four)._1),rndm)
+      case QNode(root:Coords,one,two,three,four) => (QNode(root,mapColourEffect_1(next.nextInt._2,f,one)._1,
+                                                                mapColourEffect_1(next1.nextInt._2,f,two)._1,
+                                                                mapColourEffect_1(next2.nextInt._2,f,three)._1,
+                                                                mapColourEffect_1(next3.nextInt._2,f,four)._1), rndm)
     }
   }
 
@@ -219,13 +278,13 @@ object QuadTree{
     new Color(newRed,newGreen,newBlue)
   }
 
-  private def contrastEffect(clr : Color):Color={ //128 como parametro de entrada
+  private def contrastEffect(value:Int)(clr : Color):Color={
     def factor(contrast : Int):Int={
       (259 * (contrast + 255)) / (255 * (259 - contrast))
     }
-    val newRed   = truncate(factor(128) * (clr.getRed   - 128) + 128)
-    val newGreen = truncate(factor(128) * (clr.getGreen - 128) + 128)
-    val newBlue  = truncate(factor(128) * (clr.getBlue  - 128) + 128)
+    val newRed   = truncate(factor(value) * (clr.getRed   - 128) + 128)
+    val newGreen = truncate(factor(value) * (clr.getGreen - 128) + 128)
+    val newBlue  = truncate(factor(value) * (clr.getBlue  - 128) + 128)
     new Color(newRed,newGreen,newBlue)
   }
 
@@ -257,8 +316,8 @@ object QuadTree{
     mapColourEffect(sepiaEffect,qt)
   }
 
-  def applyContrast[A](qt:QTree[A]):QTree[Coords]={
-    mapColourEffect(contrastEffect,qt)
+  def applyContrast[A](qt:QTree[A],value:Int):QTree[Coords]={
+    mapColourEffect(contrastEffect(value),qt)
   }
 
   def applyNoise[A](qt:QTree[A]):QTree[Coords]={
